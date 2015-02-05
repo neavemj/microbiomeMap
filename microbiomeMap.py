e5b65af613be54492a2043e34774c301e0c6d08e
neavemj <matthewjneave1@gmail.com>
2015-02-05 12:05:42 +0300

    default map

--- microbiomeMap.py
+++ microbiomeMap.py
@@ -5,3 +5,13 @@ __author__ = 'neavemj'
 # Matthew J. Neave 5.2.2015
 
 from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+map = Basemap(projection = 'ortho', lat_0 = 50, lon_0 = -100, resolution = 'l', area_thresh = 1000.0)
+
+map.drawcoastlines()
+
+plt.show()
+
